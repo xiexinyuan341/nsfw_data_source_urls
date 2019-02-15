9c2468da52edb5d75126ebd88d9f3e331b4bb11e
xiexinyuan <xiexinyuan341@163.com>
2019-02-15 19:57:35 +0800

    Complete Crawler.

--- image_downloader/crawler.py
+++ image_downloader/crawler.py
@@ -8,11 +8,8 @@ import requests
 
 
 class Crawler(object):
-    def __init__(self, urls_file_folder):
-        self.destination_path = Path(urls_file_folder)
-        if not self.destination_path.is_dir():
-            raise TypeError("Invalid file folder")
-        self.urls = None
+    def __init__(self, urls_iterator):
+        self.urls = urls_iterator
         self.delay_time = 1
 
     def download_image(self, url):
@@ -26,12 +23,7 @@ class Crawler(object):
                                         response.status_code)
             logging.warning(message)
 
-    def get_image_urls(self):
-        # TODO
-        pass
-
     def run(self):
-        self.get_image_urls()
         for url in self.urls:
             self.download_image(url)
             time.sleep(self.delay_time)
--- image_downloader/task_generator.py
+++ image_downloader/task_generator.py
@@ -1,9 +1,12 @@
 class TaskGenerator(object) :
-    def __init__(self):
-        self.urls = None
+    def __init__(self,root_path):
+
         pass
     # TODO
     def parse_urls_file(self):
         with open(self.urls_file) as f:
             pass
 
+    def get_image_urls(self):
+        # TODO
+        pass
